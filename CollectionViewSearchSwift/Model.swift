94d6c5685737d74a04e2ec3cac754da224aadd5d
Shripad Kulkarni <shripadkulkarnid@gmail.com>
2019-02-14 17:36:55 +0530

    added my github name

---
+++ CollectionViewSearchSwift/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  CollectionViewSearchSwift
+//
+//  Created by Samvidya Edutech LLP on 14/02/19.
+//  Copyright © 2019 VamshiKrishna. All rights reserved.
+//
+
+import Foundation
